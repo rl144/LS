6da4de1a391adb3c2b037689010d1a2b3b99a20f
rl144 <koreacnc3@gmail.com>
2015-03-05 01:51:28 +0900

    Update and rename Veigar-TTMOE to Veigar-TTMOE/Buffs.cs

---
+++ Veigar-TTMOE/Buffs.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veigar__TTMOE
+{
+    internal class NewBuff
+    {
+        public string DisplayName { set; get; }
+        public string Name { set; get; }
+        public string MenuName { set; get; }
+    }
+
+    internal class NewIgnore
+    {
+        public string DisplayName { set; get; }
+        public string Name { set; get; }
+        public string MenuName { set; get; }
+    }
+}
