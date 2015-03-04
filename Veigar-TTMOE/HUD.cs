97e21279acf5da9839e59730bde3dc0398256ed8
rl144 <koreacnc3@gmail.com>
2015-03-05 01:51:16 +0900

    Update and rename Veigar-TTMOE to Veigar-TTMOE/HUD.cs

---
+++ Veigar-TTMOE/HUD.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Veigar__TTMOE
+{
+    internal class HUD
+    {
+        public string DisplayTextON { set; get; }
+        public string DisplayTextOFF { set; get; }
+        public string MenuText { set; get; }
+        public string MenuComboText { set; get; }
+    }
+}
