cb047e7cf6c7c1837d95f2db44062de37700695e
rl144 <koreacnc3@gmail.com>
2015-05-19 13:16:09 +0900

    anti enemy spawn

--- RLProjectAutoJungle/Program.cs
+++ RLProjectAutoJungle/Program.cs
@@ -1730,6 +1730,9 @@ index = 14
                 
                 if (tminic < 1 && turret.Distance(Player.Position) <= TRRange + 250 && turret.Distance(Player.Position) > TRRange - 200)
                     Player.IssueOrder(GameObjectOrder.MoveTo, Player.Position.Extend(spawn, 855));
+                    
+                if (Player.Distance(enemy_spawn) <= TRRange + 350)
+                    Player.IssueOrder(GameObjectOrder.MoveTo, Player.Position.Extend(spawn, 855));
 
                 if (turret.Distance(Player.Position) > TRRange + 200)
                     IsAttackedByTurret = false;
